42498b473eeaa76a0720836d4093057c9df26791
otger <otger.ballester@gmail.com>
2017-03-24 09:32:21 +0100

    save as csv file functionality

---
+++ s_eventlogger/exceptions.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+exceptions
+Created by otger on 24/03/17.
+All rights reserved.
+"""
+
+
+class UnknownFileType(Exception):
+    pass
--- s_eventlogger/module.py
+++ s_eventlogger/module.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from entropyfw import Module
+from entropyfw.common import get_utc_ts, utc_ts_string
 import numbers
 
 from .callbacks import Datalog
+from .exceptions import UnknownFileType
 import os
 
 """
@@ -11,6 +13,7 @@ module
 Created by otger on 23/03/17.
 All rights reserved.
 """
+
 
 class EntropyEventLogger(Module):
     name = 'eventlogger'
@@ -44,32 +47,47 @@ class LogDataHolder(object):
 
     def add(self, event):
         if event.full_id not in self.sets:
-            self.sets[event.full_id] = LogDataSet()
+            self.sets[event.full_id] = LogDataSet(event)
         return self.sets[event.full_id].add(event)
 
     def save_to_file(self, path='/tmp'):
-        for k,v in self.sets:
+        for k, v in self.sets:
             fname = os.path.join(path, k)
             v.save_file(fname)
 
 
 class LogDataSet(object):
-    def __init__(self):
+    def __init__(self, event):
+        self.event_full_id = event.full_id
         self.items = []
         self.values = []
+        self.ts_created = get_utc_ts()
+        self.ts_last_event = event.ts
 
     def add(self, event):
         l = LogData(event)
         self.items = set(list(self.items).extend(l.values.keys()))
         self.values.append(l)
+        self.ts_last_event = event.ts
         return l
 
-    def save_to_file(self, file):
-        with open(file, 'w') as f:
+    def save_csv(self, path):
+        with open(path, 'w') as f:
+            now = get_utc_ts()
+            f.write("# CSV file for events '{0}'\n".format(self.event_full_id))
+            f.write("#   - Data set created at: {0} ({1})\n".format(utc_ts_string(self.ts_created), self.ts_created))
+            f.write("#   - Last event: {0} ({1}\n)".format(utc_ts_string(self.ts_last_event), self.ts_last_event))
+            f.write("#   - File generated at: {0} ({1}\n)".format(utc_ts_string(now), now))
+            f.write("#   - Number of events: {0}\n".format(len(self.values)))
             f.write(', '.join(str(x) for x in self.items))
             for el in self.values:
-                values = [str(el.values.get(x, '')) for x in self.items]
-                f.write('{0}, {1}'.format(el.ts, ', '.join(values)))
+                f.write(el.as_csv(items=self.items))
+
+    def save_to_file(self, path, ftype='csv'):
+        if ftype == 'csv':
+            self.save_csv(path)
+        else:
+            raise UnknownFileType("{0} is an unknown file type".format(ftype))
 
 
 class LogData(object):
@@ -84,3 +102,9 @@ class LogData(object):
 
     def __str__(self):
         return '{0}, {1}'.format(self.ts, ', '.join([str(x) for x in self.values.values()]))
+
+    def as_csv(self, separator=', ', items=None):
+        if not items:
+            items = sorted(self.values.keys())
+        values = [str(self.values.get(x, '')) for x in items]
+        return '{0}, {1}'.format(self.ts, separator.join(values))
